693ab619715752e6f13ba0f31b46db7eb8e993f9
decocampos <70168225+decocampos@users.noreply.github.com>
2023-09-13 17:19:07 -0300

    Update coletavel.py
    Coloquei o jogador e a moeda como classes

--- coletavel.py
+++ coletavel.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-import math
 
 # Inicialização do Pygame
 pygame.init()
@@ -14,17 +13,35 @@ pygame.display.set_caption('Coletor de Moedas')
 branco = (255, 255, 255)
 amarelo = (255, 255, 0)
 
+class Player:
+    def __init__(self):
+        self.rect = pygame.Rect(50, 50, 40, 40)
+        self.speed = 1.5
+
+    def mover(self, teclas):
+        if teclas[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if teclas[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        if teclas[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if teclas[pygame.K_DOWN]:
+            self.rect.y += self.speed
+
+class Moeda:
+    def __init__(self):
+        self.rect = pygame.Rect(0, 0, 20, 20)
+        self.nova_posicao()
+
+    def nova_posicao(self):
+        self.rect.x = random.randint(0, largura - 20)
+        self.rect.y = random.randint(0, altura - 20)
+
 class Jogo:
     def __init__(self):
-        self.player = pygame.Rect(50, 50, 40, 40)
-        self.player_speed = 2
-        self.moeda = pygame.Rect(0, 0, 20, 20)
+        self.player = Player()
+        self.moeda = Moeda()
         self.contagem_moedas = 0
-        self.nova_moeda()
-
-    def nova_moeda(self):
-        self.moeda.x = random.randint(0, largura - 20)
-        self.moeda.y = random.randint(0, altura - 20)
 
     def atualizar(self):
         for event in pygame.event.get():
@@ -32,37 +49,23 @@ class Jogo:
                 pygame.quit()
                 quit()
 
-        # Movimento do jogador com base nas teclas pressionadas
         teclas = pygame.key.get_pressed()
-        if teclas[pygame.K_LEFT]:
-            self.player.x -= self.player_speed
-        if teclas[pygame.K_RIGHT]:
-            self.player.x += self.player_speed
-        if teclas[pygame.K_UP]:
-            self.player.y -= self.player_speed
-        if teclas[pygame.K_DOWN]:
-            self.player.y += self.player_speed
+        self.player.mover(teclas)
 
-        # Verifica a colisão entre o jogador e a moeda
-        if self.player.colliderect(self.moeda):
+        if self.player.rect.colliderect(self.moeda.rect):
             self.contagem_moedas += 1
-            self.nova_moeda()
+            self.moeda.nova_posicao()
 
     def desenhar(self):
         tela.fill(branco)
 
-        # Desenha o jogador
-        pygame.draw.rect(tela, amarelo, self.player)
+        pygame.draw.rect(tela, amarelo, self.player.rect)
+        pygame.draw.ellipse(tela, amarelo, self.moeda.rect)
 
-        # Desenha a moeda
-        pygame.draw.ellipse(tela, amarelo, self.moeda)
-
-        # Exibe a contagem de moedas
         fonte = pygame.font.Font(None, 36)
         texto = fonte.render(f"Moedas: {self.contagem_moedas}", True, amarelo)
         tela.blit(texto, (10, 10))
 
-        # Atualiza a tela
         pygame.display.update()
 
 # Instanciar o jogo
