e7164245305687462ce8ebeae811914525e5ff62
decocampos <70168225+decocampos@users.noreply.github.com>
2023-09-18 20:33:06 -0300

    Update Moedas.py

--- Moedas.py
+++ Moedas.py
@@ -10,6 +10,9 @@ class Moeda:
     def nova_posicao(self):
         self.rect.x = random.randint(0, largura - 20)
         self.rect.y = random.randint(0, altura - 20)
+    
+    def desenhar(self, tela):
+        pygame.draw.ellipse(tela, (0, 255, 0), self.rect)
 
 class MoedaAzul: #SPEED
     def __init__(self):
@@ -20,6 +23,9 @@ class MoedaAzul: #SPEED
         self.rect.x = random.randint(0, largura - 20)
         self.rect.y = random.randint(0, altura - 20)
 
+    def desenhar(self, tela):
+        pygame.draw.ellipse(tela, (0, 0, 255), self.rect)
+
 class MoedaVermelha:
     def __init__(self):
         self.rect = pygame.Rect(0, 0, 20, 20)
@@ -28,3 +34,6 @@ class MoedaVermelha:
     def nova_posicao(self):
         self.rect.x = random.randint(0, largura - 20)
         self.rect.y = random.randint(0, altura - 20)
+
+    def desenhar(self, tela):
+        pygame.draw.ellipse(tela, (255, 0, 0), self.rect)
