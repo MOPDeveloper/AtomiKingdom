5fd5099a3c233f79cc3bbf4d9782c5f510a0a58e
decocampos <70168225+decocampos@users.noreply.github.com>
2023-09-18 21:15:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from Bombas import Bomba
 from Moedas import Moeda, MoedaAzul, MoedaVermelha
 from player import Player
+from obstaculo import Obstaculos
 
 # Inicialização do Pygame
 pygame.init()
@@ -21,39 +22,6 @@ preto = (0, 0, 0)
 clock = pygame.time.Clock()
 FPS = 90
 
-class Obstaculos:
-    def __init__(self):
-        self.tamanho_celula = 40
-        self.obstaculos = []
-        self.colunas = [160, 320, 480, 640]
-        self.linhas = [150, 300, 450]
-        self.criar_obstaculos()
-
-    def criar_obstaculos(self):
-        for coluna in self.colunas:
-            for linha in self.linhas:
-                obstaculo = pygame.Rect(coluna, linha, self.tamanho_celula, self.tamanho_celula)
-                self.obstaculos.append(obstaculo)
-
-    def desenhar_obstaculos(self, tela):
-        for obstaculo in self.obstaculos:
-            pygame.draw.rect(tela, cinza, obstaculo)
-
-            # Verifique a colisão com o jogador para impedir atravessar
-            if jogo.player.rect.colliderect(obstaculo):
-                if not jogo.colidiu_com_obstaculo:  # Verifique a colisão apenas uma vez
-                    if jogo.player.rect.left < obstaculo.right and jogo.player.rect.right > obstaculo.left:
-                        if jogo.player.rect.top < obstaculo.bottom and jogo.player.rect.bottom > obstaculo.top:
-                            if jogo.player.rect.right - obstaculo.left <= obstaculo.right - jogo.player.rect.left:
-                                jogo.player.rect.right = obstaculo.left
-                            else:
-                                jogo.player.rect.left = obstaculo.right
-                        else:
-                            if jogo.player.rect.bottom - obstaculo.top <= obstaculo.bottom - jogo.player.rect.top:
-                                jogo.player.rect.bottom = obstaculo.top
-                            else:
-                                jogo.player.rect.top = obstaculo.bottom
-                    jogo.colidiu_com_obstaculo = True
 
 """class Bomba:
     def __init__(self, x, y):
@@ -80,8 +48,6 @@ class Jogo:
         # Inicialize a lista de obstáculos usando a classe Obstaculos
         self.obstaculos = Obstaculos()
 
-        self.colidiu_com_obstaculo = False  # Variável de controle
-
     def atualizar(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -93,12 +59,15 @@ class Jogo:
                 self.bomba_ativa = True
 
         teclas = pygame.key.get_pressed()
-        if not self.colidiu_com_obstaculo:  # Verifique colisão apenas se não colidiu antes
-            self.player.mover(teclas)
-            self.player.limites(largura=largura, altura=altura)
-
-        # Quando todas as teclas são soltas, redefina a variável de controle
-        if not any(teclas):
+        self.player.mover(teclas, self.obstaculos)  # Passar obstáculos como argumento
+        self.player.limites(largura=largura, altura=altura)
+        # Verifique se o jogador colidiu com um obstáculo
+        for obstaculo in self.obstaculos.obstaculos:
+            if self.player.rect.colliderect(obstaculo):
+                self.colidiu_com_obstaculo = True
+                break
+        else:
+            # Se não houver colisão com obstáculos, redefina a variável de controle
             self.colidiu_com_obstaculo = False
 
         if self.player.rect.colliderect(self.moeda.rect):
@@ -125,7 +94,7 @@ class Jogo:
         self.moeda.desenhar(tela)
         self.moedaazul.desenhar(tela)
         self.moedavermelha.desenhar(tela)
-        self.obstaculos.desenhar_obstaculos(tela)
+        self.obstaculos.desenhar(tela)
 
         if self.bomba_ativa:
             self.bomba.desenhar(tela)
