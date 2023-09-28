59a6599c0775b0a9ba9e5e0ede5199fb69661afd
decocampos <70168225+decocampos@users.noreply.github.com>
2023-09-28 17:07:33 -0300

    Update and rename coin.py to coletaveis.py and freeze logic

---
+++ coletaveis.py
@@ -0,0 +1,34 @@
+import pygame
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, img, x, y,largura, altura):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x*largura
+        self.rect.y = y*altura
+
+        self.x = x
+        self.y = y
+
+class Freeze(pygame.sprite.Sprite):
+    def __init__(self, img, x, y,largura, altura):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x*largura
+        self.rect.y = y*altura
+
+        self.x = x
+        self.y = y
+
+class Extra_Time(pygame.sprite.Sprite):
+    def __init__(self, img, x, y,largura, altura):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x*largura
+        self.rect.y = y*altura
+
+        self.x = x
+        self.y = y
