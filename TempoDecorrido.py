1161c1696dc13925cb27449afb5e306ea643915d
Matheus Oliveira Pessoa <124061401+MOPDeveloper@users.noreply.github.com>
2023-09-29 09:53:45 -0300

    Update TempoDecorrido.py
    adicionei a def encerrado e modifiquei a atualizar, para que fosse possivel tratar quem venceu o jogo a partir da contagem de moedas quando o tempo acabar

--- TempoDecorrido.py
+++ TempoDecorrido.py
@@ -1,21 +1,25 @@
 import time
-import sys
 
-#TEMPORIZADOR DECRESCENTE
 class Temporizador:
-    def __init__(self, tempo_total): #Recebe da main o valor do tempo em segundos
+    def __init__(self, tempo_total):
         self.tempo_total = tempo_total
         self.tempo_inicial = None
+        self.encerrado = False
 
     def iniciar(self):
         self.tempo_inicial = time.time()
 
     def atualizar(self):
-        tempo_decorrido = int(self.tempo_total - (time.time() - self.tempo_inicial))
-        if tempo_decorrido == 0:
-            sys.exit()
-        return tempo_decorrido
-    
-    def aumentar(self, segundos):
-        self.tempo_total+=segundos
+        if self.tempo_inicial is not None and not self.encerrado:
+            tempo_decorrido = int(self.tempo_total - (time.time() - self.tempo_inicial))
+            if tempo_decorrido <= 0:
+                self.encerrado = True
+                return 0  # O tempo acabou
+            return tempo_decorrido
+        return 0
 
+    def aumentar(self, segundos):
+        self.tempo_total += segundos
+
+    def encerrado(self):
+        return self.encerrado
