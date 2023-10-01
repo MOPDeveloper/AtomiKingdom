c3961176796d6f47dfedab2903bbe9d19389c9e9
Mateus Ataide <121314892+ata1de@users.noreply.github.com>
2023-10-01 17:29:20 -0300

    Changed button.py

--- button.py
+++ button.py
@@ -1,23 +1,19 @@
 import pygame
 
 class Button():
-    def __init__(self, imagem, x, y):
+    def __init__(self,x, y):
+        self.image = pygame.image.load('assets/jogar_button.png')
         self.x = x
         self.y = y
-        self.image = pygame.image.load(imagem)
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
-    # COORDENADAS DO BOTÃO
-    def coord(self):
-        self.rect.topleft = self.x, self.y
-
-    
+    def show_button(self, tela):
+        tela.blit(self.image, (self.x, self.y))
+        
     def pressed(self,posx,posy,top,bottom):
         if posx in range(247, 503) and posy in range(top, bottom):
             return True
         return False
     
-    def show_button(self, place):
-        place.blit(self.image, (self.x, self.y))
-
+    
     
